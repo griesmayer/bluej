88688d124405634121301864828404f8819285cf
griesmayer <griesmayer@spengergasse.at>
2024-09-18 10:14:46 +0200

    Initial sharing of project

---
+++ Srtudent.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Srtudent here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Srtudent
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Srtudent
+     */
+    public Srtudent()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
